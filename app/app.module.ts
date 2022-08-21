e14c397b074df06cd0cbdbb80f469b771a95eb76
roysak <roysak@gmail.com>
2022-08-21 20:06:03 +0530

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
@@ -10,12 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     NgxGraphModule,
     NgxChartsModule,
     BrowserAnimationsModule,
   ],
   declarations: [AppComponent],
+  exports: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
